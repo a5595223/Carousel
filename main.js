b845741152f6e9d5c00b9423db2a2c3847495f73
frankfeng <17717444584@163.com>
2019-06-02 14:20:08 +0800

    函数封装轮播

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
 var allButtons = $('#buttons > button')
 
-for(let i = 0;i < allButtons.length;i++){
+for(let i = 0; i < allButtons.length;i++){
     $(allButtons[i]).on('click',function(x){
         var index = $(x.currentTarget).index()
-        
         var p = index * -450
-        $('#images').css({
-            transform:'translate('+p+'px)'
-        })
-        n = index
-        allButtons.eq(n)
-        .addClass('blue')
-        .siblings('.blue').removeClass('blue')
-    })
-    
-    
+      $('.images').css({
+          transform:'translate('+p+'px)'
+      })
+      console.log(n)
+      console.log(index)
+         n = index
+        activeButton(allButtons.eq(n))
+        
+ }
+    )}
+
+var n = 0
+var size = allButtons.length
+ 
+playSlide(0)
+
+
+var timeId = setTimer()
+
+function setTimer(){
+    return setInterval(()=>{
+        n += 1
+        playSlide(n%size)
+    },2000)
 }
 
-var n = 0;
-var size = allButtons.length
-allButtons.eq(n % size).trigger('click')
-.addClass('blue')
-.siblings('.blue').removeClass('blue')
+function playSlide(index){
+    allButtons.eq(index).trigger('click')
+    console.log(index)
+}
 
-var timeId = setInterval(() => {
-    n += 1
-    allButtons.eq(n % size).trigger('click')
+function activeButton($button){
+     $button 
     .addClass('blue')
     .siblings('.blue').removeClass('blue')
-},1000)
+}
 
 $('.window').on('mouseenter',function(){
     window.clearInterval(timeId)
 })
 $('.window').on('mouseleave',function(){
-    timeId = setInterval(() => {
-        n += 1
-        allButtons.eq(n % size).trigger('click')
-        .addClass('blue')
-        .siblings('.blue').removeClass('blue')
-    },1000)
+    setTimer()
 })
